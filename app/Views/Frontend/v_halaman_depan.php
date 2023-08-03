45305a0cf945a2e0f9ce5147f57c1101dbcd1948
VistapramudyaA <vistapramudiya15@gmail.com>
2023-08-04 06:16:34 +0700

    push

--- app/Views/Frontend/v_halaman_depan.php
+++ app/Views/Frontend/v_halaman_depan.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="<?= base_url('smkbudhi') ?>/css/default.css">
     <link rel="stylesheet" href="<?= base_url('smkbudhi') ?>/css/style.css">
     <link rel="stylesheet" href="<?= base_url('smkbudhi') ?>/css/responsive.css">
+    <link rel="stylesheet" href="path-to-your-font-awesome-css/font-awesome.min.css">
 </head>
 
 <body>
@@ -25,26 +26,39 @@
     $db = \Config\Database::connect();
     $jurusan = $db->table('jurusan')->get()->getResultArray();
     ?>
+
     <header id="header-part">
+
         <div class="header-top d-none d-lg-block">
             <div class="container">
-                <div class="header-contact text-lg-left text-center">
-                    <ul>
-                        <li><img src="<?= base_url('smkbudhi') ?>/images/all-icon/map.png"
-                                alt="icon"><span>JL.YOGYA-WONOSARI KM.14</span></li>
-                        <li><img src="<?= base_url('smkbudhi') ?>/images/all-icon/email.png"
-                                alt="icon"><span>smkbdbs@gmail.com</span></li>
-                    </ul>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="header-contact text-lg-left text-center">
+                            <ul>
+                                <li><img src="<?= base_url('smkbudhi') ?>/images/all-icon/map.png"
+                                        alt="icon"><span>5F6H+MRJ, Jalan Nasional III
+                                        No.Plesedan</span></li>
+                                <li><img src="<?= base_url('smkbudhi') ?>/images/all-icon/email.png"
+                                        alt="icon"><span>smkbdbs@gmail.com</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="header-opening-time text-lg-right text-center">
+                            <p>Jam Buka Sekolah : Senin sampai Sabtu - 7 Am to 4 Pm</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
         <div class="header-logo-support pt-30 pb-30">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-4 col-md-4">
                         <div class="logo">
-                            <a href="<?= base_url('Home') ?>">
-                                <img src="<?= base_url('smkbudhi') ?>/images/logosmk/logo.png" alt="Logo">
+                            <a href="index-2.html">
+                                <img src="<?= base_url('smkbudhi') ?>/images/logo.png" alt="Logo">
                             </a>
                         </div>
                     </div>
@@ -55,8 +69,8 @@
                                     <img src="<?= base_url('smkbudhi') ?>/images/all-icon/support.png" alt="icon">
                                 </div>
                                 <div class="cont">
-                                    <p>INFORMASI PENDAFTARAN</p>
-                                    <span>+6288232324437</span>
+                                    <p>Customer Service Sekolah</p>
+                                    <span>088232324437</span>
                                 </div>
                             </div>
                             <div class="button float-left">
@@ -67,23 +81,33 @@
                 </div>
             </div>
         </div>
+
         <div class="navigation">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-10 col-md-10 col-sm-9 col-8">
                         <nav class="navbar navbar-expand-lg">
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
                                 <ul class="navbar-nav mr-auto">
                                     <li class="nav-item">
-                                        <a class="nav-link" href="<?= base_url('Home') ?>"><i
-                                                class="fa fa-home"></i>Home</a>
+                                        <a class="active" href="<?= base_url('Home') ?>">Home</a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link" href="#">Jurusan</a>
                                         <ul class="sub-menu">
                                             <?php foreach ($jurusan as $key => $data) { ?>
-                                            <li><a
-                                                    href="<?= base_url('Home/Jurusan/'. $data['id_jurusan']) ?>"><?= $data['jurusan'] ?></a>
+                                            <li>
+                                                <a href="<?= base_url('Home/Jurusan/' . $data['id_jurusan']) ?>">
+                                                    <?= $data['jurusan'] ?>
+                                                </a>
                                             </li>
                                             <?php } ?>
                                         </ul>
@@ -98,24 +122,54 @@
                                         <a class="nav-link"
                                             href="<?= base_url('Home/Ekstrakurikuller') ?>">Ekstrakurikuller</a>
                                     </li>
-                                </ul>
                             </div>
                         </nav>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-3 col-4">
+                        <div class="right-icon text-right">
+                            <ul>
+                                <li><a href="#" id="search"><i class="fa fa-search"></i></a></li>
+                                <li>
+                                    <a href="<?= base_url('Auth') ?>" style="font-size: 1.5em;">
+                                        <i class="fa fa-sign-in"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </header>
+
+    <div class="search-box">
+        <div class="serach-form">
+            <div class="closebtn">
+                <span></span>
+                <span></span>
+            </div>
+            <form action="#">
+                <input type="text" placeholder="Search by keyword">
+                <button><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+    </div>
+
     <section class="content">
         <?php if ($page) {
             echo view($page);
         } ?>
     </section>
+
     <footer class="header-top d-none d-lg-block">
         <div class="text-center p-3">
             <strong><a class="text-white">V1 2023-"SMK BUDHI DHARMA PIYUNGAN"</a></strong>
         </div>
     </footer>
+
+    <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="<?= base_url('smkbudhi') ?>/js/vendor/modernizr-3.6.0.min.js"></script>
     <script src="<?= base_url('smkbudhi') ?>/js/vendor/jquery-1.12.4.min.js"></script>
     <script src="<?= base_url('smkbudhi') ?>/js/bootstrap.min.js"></script>
--- app/Views/IsiFrontend/v_home.php
+++ app/Views/IsiFrontend/v_home.php
@@ -24,3 +24,159 @@ $images = [
     </div>
     <?php endforeach; ?>
 </section>
+
+<section id="category-part">
+    <div class="container">
+        <div class="category pt-40 pb-80">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="category-text pt-40">
+                        <h2>Sekolah Terbaik DiJogja</h2>
+                    </div>
+                </div>
+                <div class="col-lg-6 offset-lg-1 col-md-8 offset-md-2 col-sm-8 offset-sm-2 col-8 offset-2">
+                    <div class="row category-slied mt-40">
+                        <div class="col-lg-4">
+                            <a href="#">
+                                <span class="singel-category text-center color-1">
+                                    <span class="icon">
+                                        <img src="<?= base_url('smkbudhi') ?>/images/all-icon/ctg-1.png" alt="Icon">
+                                    </span>
+                                    <span class="cont">
+                                        <span>Multi Bahasa</span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-lg-4">
+                            <a href="#">
+                                <span class="singel-category text-center color-2">
+                                    <span class="icon">
+                                        <img src="<?= base_url('smkbudhi') ?>/images/all-icon/ctg-2.png" alt="Icon">
+                                    </span>
+                                    <span class="cont">
+                                        <span>Entrepreneur</span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-lg-4">
+                            <a href="#">
+                                <span class="singel-category text-center color-3">
+                                    <span class="icon">
+                                        <img src="<?= base_url('smkbudhi') ?>/images/all-icon/ctg-3.png" alt="Icon">
+                                    </span>
+                                    <span class="cont">
+                                        <span>Studi Pustaka</span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-lg-4">
+                            <a href="#">
+                                <span class="singel-category text-center color-1">
+                                    <span class="icon">
+                                        <img src="<?= base_url('smkbudhi') ?>/images/all-icon/ctg-1.png" alt="Icon">
+                                    </span>
+                                    <span class="cont">
+                                        <span>Bahasa</span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-lg-4">
+                            <a href="#">
+                                <span class="singel-category text-center color-2">
+                                    <span class="icon">
+                                        <img src="<?= base_url('smkbudhi') ?>/images/all-icon/ctg-2.png" alt="Icon">
+                                    </span>
+                                    <span class="cont">
+                                        <span>Entrepreneur</span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-lg-4">
+                            <a href="#">
+                                <span class="singel-category text-center color-3">
+                                    <span class="icon">
+                                        <img src="<?= base_url('smkbudhi') ?>/images/all-icon/ctg-3.png" alt="Icon">
+                                    </span>
+                                    <span class="cont">
+                                        <span>Perpustakaan</span>
+                                    </span>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="about-part" class="pt-65">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5">
+                <div class="section-title mt-50">
+                    <h5>Profil Sekolah</h5>
+                    <h2>SMK Budhi Dharma Piyungan Bantul</h2>
+                </div>
+                <div class="about-cont">
+                    <p>SMK Budhi Dharma Piyungan adalah lembaga pendidikan menengah kejuruan yang terletak di Kabupaten
+                        Bantul, Provinsi Daerah Istimewa Yogyakarta. Tujuan utama sekolah ini adalah memberikan
+                        pendidikan dan pelatihan kejuruan kepada siswa-siswi agar siap memasuki dunia kerja setelah
+                        lulus. Dengan visi menjadi lembaga pendidikan kejuruan unggul dalam pembentukan karakter dan
+                        keterampilan berbasis teknologi, serta misi untuk menyelenggarakan pendidikan kejuruan
+                        berkualitas sesuai perkembangan teknologi, mengembangkan kompetensi siswa dalam bidang akademik
+                        dan praktik, mendorong pembentukan karakter berlandaskan nilai-nilai keagamaan dan kebajikan,
+                        serta menjalin kemitraan dengan dunia industri guna meningkatkan peluang kerja bagi lulusan.</p>
+                    <a href="#" class="main-btn mt-55">Profil Sekolah</a>
+                </div>
+            </div>
+            <!-- <div class="col-lg-6 offset-lg-1">
+                <div class="about-event mt-50">
+                    <div class="event-title">
+                        <h3>Upcoming events</h3>
+                    </div>
+                    <ul>
+                        <li>
+                            <div class="singel-event">
+                                <span><i class="fa fa-calendar"></i> 2 December 2018</span>
+                                <a href="events-singel.html">
+                                    <h4>Campus clean workshop</h4>
+                                </a>
+                                <span><i class="fa fa-clock-o"></i> 10:00 Am - 3:00 Pm</span>
+                                <span><i class="fa fa-map-marker"></i> Rc Auditorim</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="singel-event">
+                                <span><i class="fa fa-calendar"></i> 2 December 2018</span>
+                                <a href="events-singel.html">
+                                    <h4>Tech Summit</h4>
+                                </a>
+                                <span><i class="fa fa-clock-o"></i> 10:00 Am - 3:00 Pm</span>
+                                <span><i class="fa fa-map-marker"></i> Rc Auditorim</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="singel-event">
+                                <span><i class="fa fa-calendar"></i> 2 December 2018</span>
+                                <a href="events-singel.html">
+                                    <h4>Enviroement conference</h4>
+                                </a>
+                                <span><i class="fa fa-clock-o"></i> 10:00 Am - 3:00 Pm</span>
+                                <span><i class="fa fa-map-marker"></i> Rc Auditorim</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div> -->
+            <div class="about-bg">
+                <img src="<?= base_url('smkbudhi') ?>/images/sekolah3.jpg" alt="About">
+            </div>
+</section>
